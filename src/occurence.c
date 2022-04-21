7b0e030f7210387c341c33bc75598575ae799aa3
kevin42000 <93095916+kevin42000@users.noreply.github.com>
2022-04-21 23:08:36 +0200

    Create occurence.c

---
+++ src/occurence.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void affiche_une_partie(char fichier[]){
+  char lecture[100];
+  FILE *p;
+  p = fopen(fichier, "r");
+  fread(lecture, 99, 1, fichier);
+  lecture[99] = '\0';
+  printf("%s\n", lecture);
+  fclose(p);
+}
+
+void affiche_la_totalite(char fichier[]){
+  char caractere;
+  FILE *p;
+  p = fopen(fichier, "r");
+  do{
+    caractere = fgetc(p);
+    if(feof(p)){
+      break;
+    }
+    printf'"%c", caractere);
+  }while(caractere != EOF);
+  fclose(p);
+  printf("\n");
+}
+       
+void occurence(FILE *fic, int tab[256]){
+  char caractere;
+  do{
+    caractere = fgetc(p);
+    if(feof(p)){
+      break;
+    }
+    tab[caractere]++;
+  }while(caractere != EOF);
+}
