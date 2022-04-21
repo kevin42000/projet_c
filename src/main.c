1ad9069ffcf30ce373fd6da4687a9fc2d3ad5006
kevin42000 <93095916+kevin42000@users.noreply.github.com>
2022-04-21 22:46:55 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argv, char *argc[]){
-  
+int main(int argc, char *argv[]){
+  if(argc == 1){
+    
+  }else if(argv[1] == "-c"){
+    
+  }else if(argv[1] == "-d"){
+    
+  }else if(argv[1] == "-h"){
+    
+  }
   exit(EXIT_SUCCESS);
 }
