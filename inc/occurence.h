603450319b40f47beac689aee42951696803f74b
kevin42000 <93095916+kevin42000@users.noreply.github.com>
2022-04-21 23:11:15 +0200

    Create occurence.h

---
+++ inc/occurence.h
@@ -0,0 +1,10 @@
+#ifndef DEF_OCCURENCE
+#def DEF_OCCURENCE
+
+void afficher_une_partie(char fichier[]);
+
+void afficher_la_totalite(char fichier[]);
+
+void occurence(FILE *fic, int tab[256]);
+
+#endif
