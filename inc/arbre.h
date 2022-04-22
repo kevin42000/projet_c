75a648b8312eae0bc81db59bf33efecca5a3f9cf
kevin42000 <93095916+kevin42000@users.noreply.github.com>
2022-04-22 15:14:04 +0200

    Update arbre.h

--- inc/arbre.h
+++ inc/arbre.h
@@ -10,4 +10,10 @@ typedef struct noeud{
   struct noeud droit;
 }noeud;
 
+noeud *creation_noeud[256](int tab[256]);
+
+noeud * creer_feuille(int *tab, int index);
+
+void parcours_tableau(noeud *arbre_huffman[256], int *x, int *y);
+
 #endif
