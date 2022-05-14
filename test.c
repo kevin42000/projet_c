76aafabd75626233671d2d4ba623464281bd714e
nico78916 <nico789.rf@gmail.com>
2022-05-14 19:43:00 +0200

    a

--- test.c
+++ test.c
@@ -1,69 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "inc/pile.h"
-#define MAX_CHAR 32
-#define MAX_BYTES 256
-#define DEBUG 0
-#include <math.h>
-
-char *random_bytes(int count){
+int gen_file(char* name,int size,int nb_char){
+    FILE *f = fopen(name,"w");
+    char c;
+    char progress[23];
     int i;
-    char* tab = calloc(sizeof(char),count);
-    srand(time(NULL));
-    for(i=0;i<count;i++){
-        tab[i] = '0' + rand() % 0x2;
-    }
-    return tab;
-}
-
-void afficher_octet(int b){
-    int i;
-    for(i=0;i<8;i++){
-        printf("%d",!!((b << i) & 0x80));
-        /* 
-        0x80 = 128 (l'hexa est plus stylé je trouve)
-        on prend l'inverse de l'inverse du du décalage de i en operant un AND avec 128 (1000 0000)
-        on affiche donc l'octet voulut
-        */
-    }
-    printf("\n");
-}
-
-
-
-int nb_bits = 3;
-int main(int argc,char *argv[]){
-    FILE *f;
-    pile p = pile_vide();
-    int i,j,x;
-    char*tab = random_bytes(MAX_CHAR);
-    char b;
-    if(argc < 0x2)return 0;
-    fclose(fopen(argv[1],"wb"));
-    f = fopen(argv[1],"ab");
-    for(i=0;i<MAX_CHAR;i++){
-        printf("%c",tab[i]);
-        p = empiler(p,tab[i]-'0');
-        if(taille_pile(p) == 0x8){
-            puts("c");
-            for(j=0;j<0x8;j++){
-                x = sommet(p);
-                p = depiler(p);
-                if(x == 0){
-                    b <<= 1;
-                }else{
-                    b = ( b << 1 ) + 1;
-                }
-            }
-            afficher_octet(b);
-            fwrite(&b,sizeof(char),1,f);
-            b = 0;
+    int max_size = size;
+    double al = ((double)(max_size-size)/(double)max_size)*100.0;
+    printf("Starting creating %s with %d chars and length of %d\n",name,nb_char,size);
+    progress[0] = '[';
+    progress[21] = ']';
+    progress[22] = '\0';
+    while(size > 0){
+        if(rand()%100 == 0 && nb_char > 2){
+            c = '\n';
+        }else{
+            c = rand() % nb_char + 32;
         }
+        fputc(c,f);
+        size--;
+        al = ((double)(max_size-size)/(double)max_size)*100.0;
+        for(i=1;i<=20;i++){
+            progress[i] = (5.0*i-1) < al ? '*' : '-';
+        }
+        printf("\r %.1f%% %s",al,progress);
+        fflush(stdout);
     }
-    printf("\n");
-    printf("Taille pile = %d\n",taille_pile(p));
     fclose(f);
-    f = fopen(argv[1],"rb");
-    fread(&b,sizeof(char),1,f);
+}
+int main(int c,char *v[]){
+    int x,i,j;
+    char filename[12] = "test000.txt";
+    srand(time(NULL));
+    if(c < 2){
+        printf("Usage : %s <nombre de fichier à créer>\n",v[0]);
+        exit(-1);
+    }
+    x = atoi(v[1]);
+    for(i=0;i<x;i++){
+        sprintf(filename,"test%d.txt",i);
+        j = rand()%(RAND_MAX/100);
+        printf("Creating a file of %d chars\n",j);
+        gen_file(filename,j,2);
+        printf("\n");
+    }
 }
